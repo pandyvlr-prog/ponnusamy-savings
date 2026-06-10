e81cb1cd0216594826b43711364e4fc3952f2272
Aravind <aravind@example.com>
2026-06-10 11:43:30 +0530

    Fixed missing quotes syntax error in console log

--- app.js
+++ app.js
@@ -4194,7 +4194,7 @@ if (btnPwaInstall) {
             deferredPrompt.prompt();
             // Wait for the user to respond to the prompt
             const { outcome } = await deferredPrompt.userChoice;
-            console.log(User response to the install prompt:  + outcome);
+            console.log('User response to the install prompt: ' + outcome);
             // We've used the prompt, and can't use it again, throw it away
             deferredPrompt = null;
         }
